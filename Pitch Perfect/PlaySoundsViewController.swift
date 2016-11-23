518cf412ed4022875c3b1437db08862194e8a4e5
Richard Reed <rich_mack9@yahoo.com>
2016-11-22 20:03:25 -0600

    Automated Swift 3.0 conversion complete

--- Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -27,15 +27,15 @@ class PlaySoundsViewController: UIViewController {
         super.viewDidLoad()
         
         audioEngine = AVAudioEngine()
-        audioFile = try! AVAudioFile(forReading: receivedAudio.filePathUrl)
+        audioFile = try! AVAudioFile(forReading: receivedAudio.filePathUrl as URL)
         
-        audioPlayer = try! AVAudioPlayer(contentsOfURL: receivedAudio.filePathUrl)
+        audioPlayer = try! AVAudioPlayer(contentsOf: receivedAudio.filePathUrl as URL)
         audioPlayer.enableRate = true
         
     }
     
-    override func viewWillAppear(animated: Bool) {
-        stopButton.hidden = true
+    override func viewWillAppear(_ animated: Bool) {
+        stopButton.isHidden = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -44,14 +44,14 @@ class PlaySoundsViewController: UIViewController {
     }
     
 
-    @IBAction func playbackRecording(sender: UIButton) {
+    @IBAction func playbackRecording(_ sender: UIButton) {
         resetPlayer()
         audioPlayer.currentTime = 0
-        stopButton.hidden = false
-        slowButton.enabled = false
-        fastButton.enabled = false
-        chipmunkButton.enabled = false
-        darthVaderButton.enabled = false
+        stopButton.isHidden = false
+        slowButton.isEnabled = false
+        fastButton.isEnabled = false
+        chipmunkButton.isEnabled = false
+        darthVaderButton.isEnabled = false
         
         if let playback = Playback(rawValue: sender.tag) {
             switch (playback) {
@@ -65,39 +65,39 @@ class PlaySoundsViewController: UIViewController {
         
     }
 
-    @IBAction func stopPlayback(sender: UIButton) {
+    @IBAction func stopPlayback(_ sender: UIButton) {
         
         resetPlayer()
         
-        stopButton.hidden = true
-        slowButton.enabled = true
-        fastButton.enabled = true
-        chipmunkButton.enabled = true
-        darthVaderButton.enabled = true
+        stopButton.isHidden = true
+        slowButton.isEnabled = true
+        fastButton.isEnabled = true
+        chipmunkButton.isEnabled = true
+        darthVaderButton.isEnabled = true
     }
     
-    func playAudioWithVariableSpeed(speed: Float){
+    func playAudioWithVariableSpeed(_ speed: Float){
         resetPlayer()
         audioPlayer.rate = speed
         audioPlayer.play()
         
     }
     
-    func playAudioWithVariablePitch(pitch: Float){
+    func playAudioWithVariablePitch(_ pitch: Float){
         
         resetPlayer()
         
         let audioPlayerNode = AVAudioPlayerNode()
-        audioEngine.attachNode(audioPlayerNode)
+        audioEngine.attach(audioPlayerNode)
         
         let changePitchEffect = AVAudioUnitTimePitch()
         changePitchEffect.pitch = pitch
-        audioEngine.attachNode(changePitchEffect)
+        audioEngine.attach(changePitchEffect)
         
         audioEngine.connect(audioPlayerNode, to: changePitchEffect, format: nil)
         audioEngine.connect(changePitchEffect, to: audioEngine.outputNode, format: nil)
         
-        audioPlayerNode.scheduleFile(audioFile, atTime: nil, completionHandler: nil)
+        audioPlayerNode.scheduleFile(audioFile, at: nil, completionHandler: nil)
         try! audioEngine.start()
         
         audioPlayerNode.play()
--- Pitch Perfect/RecordSoundsViewController.swift
+++ Pitch Perfect/RecordSoundsViewController.swift
@@ -18,13 +18,13 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
     var audioRecorder: AVAudioRecorder!
     var recordedAudio: RecordedAudio!
     
-    override func viewWillAppear(animated: Bool) {
+    override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         // initial set up of recording view
-        stopButton.hidden = true
-        recordButton.enabled = true
-        recordingLabel.hidden = false
-        recordingLabel.textColor = UIColor.lightGrayColor()
+        stopButton.isHidden = true
+        recordButton.isEnabled = true
+        recordingLabel.isHidden = false
+        recordingLabel.textColor = UIColor.lightGray
         recordingLabel.text = "Tap microphone to record"
     }
 
@@ -33,55 +33,55 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func startRecording(sender: UIButton) {
+    @IBAction func startRecording(_ sender: UIButton) {
         
-        recordingLabel.textColor = UIColor.redColor()
+        recordingLabel.textColor = UIColor.red
         recordingLabel.text = "recording in progress..."
-        stopButton.hidden = false
-        recordButton.enabled = false
+        stopButton.isHidden = false
+        recordButton.isEnabled = false
         
         // setting up audio file to save recording
-        let dirPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as String
+        let dirPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as String
         
         let recordingName = "my_audio.wav"
         let pathArray = [dirPath, recordingName]
-        let filePath = NSURL.fileURLWithPathComponents(pathArray)
+        let filePath = URL.fileURL(withPathComponents: pathArray)
         print(filePath)
         
         let session = AVAudioSession.sharedInstance()
         try! session.setCategory(AVAudioSessionCategoryPlayAndRecord)
         
-        try! audioRecorder = AVAudioRecorder(URL: filePath!, settings: [:])
+        try! audioRecorder = AVAudioRecorder(url: filePath!, settings: [:])
         audioRecorder.delegate = self
-        audioRecorder.meteringEnabled = true
+        audioRecorder.isMeteringEnabled = true
         audioRecorder.prepareToRecord()
         audioRecorder.record()
         
     }
     
-    func audioRecorderDidFinishRecording(recorder: AVAudioRecorder, successfully flag: Bool) {
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
         if(flag) {
             recordedAudio = RecordedAudio(filePathUrl: recorder.url, title: recorder.url.lastPathComponent!)
-            performSegueWithIdentifier("stopRecording", sender: recordedAudio)
+            performSegue(withIdentifier: "stopRecording", sender: recordedAudio)
         } else {
             print("Recording was not successful")
-            recordButton.enabled = true
-            stopButton.hidden = true
+            recordButton.isEnabled = true
+            stopButton.isHidden = true
         }
     }
     
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if (segue.identifier == "stopRecording") {
-            let playSoundsVC: PlaySoundsViewController = segue.destinationViewController as! PlaySoundsViewController
+            let playSoundsVC: PlaySoundsViewController = segue.destination as! PlaySoundsViewController
             let data = sender as! RecordedAudio
             playSoundsVC.receivedAudio = data
         }
     }
 
-    @IBAction func stopRecording(sender: UIButton) {
+    @IBAction func stopRecording(_ sender: UIButton) {
         
-        recordingLabel.hidden = true
-        stopButton.hidden = true
+        recordingLabel.isHidden = true
+        stopButton.isHidden = true
         
         audioRecorder.stop()
         let audioSession = AVAudioSession.sharedInstance()
